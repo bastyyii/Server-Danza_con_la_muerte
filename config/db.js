e68aa0a3de1c17d3afbc82de98971109e8838f13
bastyyii <bastivaldi15@gmail.com>
2022-05-06 03:34:01 -0400

    make it better

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config({path: 'variables.env'});
 
 
-
+//Conexion
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.DB_MONGO, {});
